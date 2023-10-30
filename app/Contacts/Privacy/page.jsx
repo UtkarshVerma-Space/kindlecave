a71a6397a082a59781a0d8ed319bf1cb5b824658
UtkarshVerma-Coder <utkarshverma001@outlook.com>
2023-10-30 14:48:01 +0530

    Added T&C Page Privacy Policy Refund and Re

--- app/Contacts/Privacy/page.jsx
+++ app/Contacts/Privacy/page.jsx
@@ -31,7 +31,7 @@ export default function page() {
     <li>Find and prevent fraud</li>
 </p>
 <h2>Log Files</h2>
-<p>kindlecave follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol IP addresses, browser type, Internet Service Provider ISP, date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+<p>kindlecave follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services analytics. The information collected by log files include internet protocol IP addresses, browser type, Internet Service Provider ISP, date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users movement on the website, and gathering demographic information.</p>
 
 
 
@@ -43,14 +43,14 @@ export default function page() {
 
 <p>You may consult this list to find the Privacy Policy for each of the advertising partners of kindlecave.
 
-Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on kindlecave, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on kindlecave, which are sent directly to users browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
 
 Note that kindlecave has no access to or control over these cookies that are used by third-party advertisers.</p>
 
 <h2>Third Party Privacy Policies</h2>
 <p>kindlecave&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
 
-You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
+You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers respective websites.</p>
 
 <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
 <p>Under the CCPA, among other rights, California consumers have the right to:</p>
